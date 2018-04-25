edb48dba1b137c2cf440b69ceb0c6c8ba209f9a5
Vũ Cường <phantom.yi@hotmail.com>
2018-04-25 08:29:39 +0700

    Cuongvd
    TESSST

---
+++ Angular/home/home.controller.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['UserService', '$rootScope', '$scope'];
+    function HomeController(UserService, $rootScope, $scope) {
+        var vm = this;
+
+        vm.user = null;
+        vm.allUsers = [];
+        vm.deleteUser = deleteUser;
+        vm.updateUser = updateUser;
+        //$scope.isUpdate = true;
+
+        initController();
+        function initController() {
+            loadCurrentUser();
+            loadAllUsers();
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                    vm.user = user;
+                });
+        }
+
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
+
+        function deleteUser(id) {
+            UserService.Delete(id)
+            .then(function () {
+                loadAllUsers();
+            });
+        }
+    }
+
+})();
